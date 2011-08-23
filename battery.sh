5fc5024fc32e376db9e3c24f19cc11406e908045
masahino <masahino@gmail.com>
2011-08-23 21:38:02 +0900

    stop to use LegacyBatteryInfo

--- battery.sh
+++ battery.sh
@@ -1,5 +1,13 @@
-info=( `ioreg -w0 -l |grep LegacyBatteryInfo | tr '[\"\=\(\{\)\}\,|]' ' ' ` )
-capacity=${info[6]}
-current=${info[8]}
-percent=`expr 100 \* $current \/ $capacity`
-echo "${percent}%"
+#!/bin/sh
+
+#info=( `ioreg -w0 -l |grep LegacyBatteryInfo | tr '[\"\=\(\{\)\}\,|]' ' ' ` )
+#capacity=${info[6]}
+#current=${info[8]}
+#percent=`expr 100 \* $current \/ $capacity`
+#echo "${percent}%"
+
+ioreg -n AppleSmartBattery  -r | \
+awk 'BEGIN{FS="="; max = 0; current = 0;}
+$1~/MaxCapacity/ {max = $2}
+$1~/CurrentCapacity/ {current = $2}
+END { printf("%d%%", 100 * current/max); }'
